cbb53d69b32f4824cd386342ff38ea138833a1da
Takahiro Hirata <ta.hirata@opt.ne.jp>
2020-06-08 15:21:51 +0900

    add: test HelloController

--- hello-world/build.sbt
+++ hello-world/build.sbt
@@ -3,3 +3,7 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.13.2"
 
 libraryDependencies += guice
+
+libraryDependencies ++= Seq(
+  "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % "test"
+)
---
+++ hello-world/test/controllers/HelloControllerSpec.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import org.scalatestplus.play.PlaySpec
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.api.test.Helpers
+
+class HelloControllerSpec extends PlaySpec {
+
+  def controller = new HelloController(Helpers.stubControllerComponents())
+
+  "get" should {
+    "クエリーパラメータがある場合は「Hello, namae!」というレスポンスを返す" in {
+      val name = "namae"
+      val result = controller.get(Some(name))(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === s"Hello, $name!")
+    }
+
+    """クエリーパラメータがない場合は「Please give a name as a query parameter named "name".」というレスポンスを返す""" in {
+      val result = controller.get(None)(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === """Please give a name as a query parameter named "name".""")
+    }
+  }
+}
